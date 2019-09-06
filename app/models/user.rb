5ba71390a82706fba954df1c2a77b5a7304fd217
ValterMed <waltermedinapuy@live.com>
2019-09-06 13:58:24 -0500

    files modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User
   field :name, type: String
   field :last_name, type: String
   field :mobile_phone, type: String
+  field :work_place, type: String
+
+  #validates :email, presence: true
+  #validates :name, presence: true
+  validates_presence_of :email, :name
+  validates :email, uniqueness: true
+
+  def full_name
+    "#{name} #{last_name}"
+  end
+
 end
